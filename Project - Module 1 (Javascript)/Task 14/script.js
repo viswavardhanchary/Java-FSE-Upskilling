1678fbef1f41a57605d6febe7d58c88639cde904
Viswa vardhan chary <viswavardhanchary@gmail.com>
2026-06-01 23:04:27 +0530

    Completed Module 1 (JavaScript) Task 14

---
+++ Project - Module 1 (Javascript)/Task 14/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $("#registerBtn").click(function () {
+
+        alert("Registered Successfully");
+
+    });
+
+    $("#showBtn").click(function () {
+
+        $("#eventCard").fadeIn();
+
+    });
+
+    $("#hideBtn").click(function () {
+
+        $("#eventCard").fadeOut();
+
+    });
+
+});
