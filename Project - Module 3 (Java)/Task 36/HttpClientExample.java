8a1c67ed8ea8fa5b58e6bd2dac4eb080b8ba38e7
Viswa vardhan chary <viswavardhanchary@gmail.com>
2026-06-04 00:13:49 +0530

    Completed Module 3 (Java) Task 36

---
+++ Project - Module 3 (Java)/Task 36/HttpClientExample.java
@@ -0,0 +1,22 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HttpClientExample {
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.github.com"))
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Status Code: " + response.statusCode());
+            System.out.println("Response Body: " + response.body());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
