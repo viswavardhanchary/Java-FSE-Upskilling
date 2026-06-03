3bde6577f03ff360c3f6892ac190de2be93ac076
Viswa vardhan chary <viswavardhanchary@gmail.com>
2026-06-03 20:09:49 +0530

    Completed Module 3 (Java) Task 6

---
+++ Project - Module 3 (Java)/Task 6/DataTypes.java
@@ -0,0 +1,17 @@
+
+public class DataTypes {
+    public static void main(String[] args) {
+       int a = 10;
+       float b = 10.5f;
+       char ch = 'a';
+       boolean c = false;
+       double d = 9.6;
+       System.out.println("Integer a = " + a);
+       System.out.println("Float b = " + b);
+       System.out.println("Character ch = " + ch);
+       System.out.println("Boolean c = " + c);
+       System.out.println("Double d = " + d);
+
+       
+    }
+}
