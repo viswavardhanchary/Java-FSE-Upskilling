8328e3a8d6a3604f4d737d569dcae047c11f2c55
Viswa vardhan chary <viswavardhanchary@gmail.com>
2026-06-03 20:23:36 +0530

    Completed Module 3 (Java) Task 8

---
+++ Project - Module 3 (Java)/Task 8/OperatorEvaluation.java
@@ -0,0 +1,17 @@
+public class OperatorEvaluation {
+    public static void main(String[] args) {
+        int a = 10*2 + 5;
+        System.out.println("Expression is : 10*2+5");
+        System.out.println("Evalution: ");
+        System.out.println("First 10*2 = 20");
+        System.out.println("second 20+5 = 25");
+        System.out.println("Third a = 25");
+        System.out.println("Results is a = " +a);
+    }
+    
+    
+    
+   
+
+
+}
