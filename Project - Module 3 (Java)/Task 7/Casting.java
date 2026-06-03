4268b3472113cc85768e906c00ab9df0f0d95b12
Viswa vardhan chary <viswavardhanchary@gmail.com>
2026-06-03 20:17:23 +0530

    Completed Module 3 (Java) Task 7

---
+++ Project - Module 3 (Java)/Task 7/Casting.java
@@ -0,0 +1,11 @@
+public class Casting {
+    public static void main(String[] args) {
+       double b = 10.6;
+       int cb = (int)10.6;
+       int a = 10;
+       double ca = a;
+       System.out.println("Converted double " + b + " to Integer " + cb);
+       System.out.println("Converted Integer " + a + " to Double " + ca);
+       
+    }
+}
